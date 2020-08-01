b3a24fa4d35b6d112c653eec1046c50ff84e1180
lisyuan <lisyuan0429@gmail.com>
2020-08-02 01:07:19 +0800

    finish unigram, bigram, trigram

--- model/model.py
+++ model/model.py
@@ -6,26 +6,26 @@ class CoAttent(nn.Module):
 
     def __init__(self, num_vocab, embbed_size=512):
 
+        super(CoAttent, self).__init__()
+        self.num_vocab = num_vocab
+
         self.embedding = nn.Embedding(num_vocab, embbed_size)
         self.unigram = nn.Conv1d(embbed_size, embbed_size, kernel_size=1, stride=1, padding=0)
         self.bigram = nn.Conv1d(embbed_size, embbed_size, kernel_size=2, stride=1, padding=1)
-        self.trigram = nn.Conv1d(embbed_size, embbed_size, kernel_size=2, stride=1, padding=2)
+        self.trigram = nn.Conv1d(embbed_size, embbed_size, kernel_size=3, stride=1, padding=1)
 
-    def forward(self, input):
+    def forward(self, question):
 
-        pass
+        # embedding = self.embedding(question)
+        a = self.bigram(question).narrow(2, 1, self.num_vocab)  # get 1: num_vocab+1 vector
 
-torch.manual_seed(0)
-conv1 = nn.Conv1d(in_channels=5, out_channels=2, padding=0, kernel_size=2, dilation=1)
-input = torch.ones(1,3,5)
-print(input)
-# batch_size x text_len x embedding_size -> batch_size x embedding_size x text_len
-input = input.permute(0,2,1)
-out = conv1(input)
-print(conv1.weight)
-print('sum0:',torch.sum(conv1.weight[0]))
-print('sum1:',torch.sum(conv1.weight[1]))
+        return a
+if __name__ == '__main__':
 
-print(conv1.bias)
-print(out.size())
-print(out)
+    torch.manual_seed(0)
+
+    test_model = CoAttent(num_vocab=10)
+    input = torch.ones(1,10,512)  # 10 words, 512 vector
+    input = input.permute(0,2,1) # 1 x 512 x 10
+    output = test_model(input)
+    print(output.size())
